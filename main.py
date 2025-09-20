5657573c6ffb9ee38afd5367207e57aa8098a9f0
hitsuji <kyoukarada@gmail.com>
2025-09-20 09:54:47 +0900

    fix: index

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Cloud Run Flask App</h1><p>デプロイが成功しました！</p>"
+    return "<h1>Cloud Run Flask App</h1><p>修正しました！</p>"
 
 @app.route('/health')
 def health():
